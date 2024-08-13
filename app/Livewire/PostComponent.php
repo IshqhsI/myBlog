9f4439544724fb854b96b29e9308266174ba6c42
IshqhsI <oneridh18@gmail.com>
2024-08-13 21:22:58 +0800

    update post component table & add method for search

--- app/Livewire/PostComponent.php
+++ app/Livewire/PostComponent.php
@@ -8,11 +8,17 @@ use App\Models\Post;
 class PostComponent extends Component
 {
     public $posts = [];
+    public $search = '';
     public function render()
     {
-        $this->posts = Post::all();
+        $this->posts = Post::where('title', 'like', '%' . $this->search . '%')->get();
         return view('livewire.post-component', [
             'posts' => $this->posts
         ]);
+    }
+
+    public function searchPost()
+    {
+        $this->posts = Post::where('title', 'like', '%' . $this->search . '%')->orWhere('content', 'like', '%' . $this->search . '% ')->get();
     }
 }
--- resources/views/livewire/post-component.blade.php
+++ resources/views/livewire/post-component.blade.php
@@ -26,54 +26,64 @@
         </div>
         <div class="w-1/2 text-end">
             {{-- stiling form --}}
-            <input type="text" class="form-control w-1/2 rounded p-2 border-blue-600" placeholder="Search" wire:model="search">
+            <input type="text" class="form-control w-1/2 rounded p-2 border-blue-600" placeholder="Search" wire:keydown="searchPost" wire:model="search">
         </div>
     </div>
 
     {{-- table data --}}
-    <table class="min-w-full bg-white shadow-md rounded-lg overflow-hidden dark:bg-gray-800 dark:text-gray-100 mt-5">
-        <thead class="bg-blue-600 text-white dark:bg-blue-700">
-            <tr>
-                <th class="py-3 px-6 text-left">No. </th>
-                <th class="py-3 px-6 text-left">Title</th>
-                <th class="py-3 px-6 text-left">Slug</th>
-                <th class="py-3 px-6 text-left">Content</th>
-                <th class="py-3 px-6 text-left">Image</th>
-                <th class="py-3 px-6 text-left">Category</th>
-                <th class="py-3 px-6 text-left">User</th>
-                <th class="py-3 px-6 text-left">Action</th>
-            </tr>
-        </thead>
-        <tbody>
-            {{-- Pagination --}}
-            @foreach ($posts as $i => $post)
-                <tr class="border-b dark:border-gray-600">
-                    <td class="py-3 px-6 text-left">{{ $i + 1 }}</td>
-                    <td class="py-3 px-6 text-left">{{ $post->title }}</td>
-                    <td class="py-3 px-6 text-left">{{ $post->slug }}</td>
-                    <td class="py-3 px-6 text-left">{{ $post->content }}</td>
-                    <td class="py-3 px-6 text-left">{{ $post->image }}</td>
-                    <td class="py-3 px-6 text-left">{{ $post->category->name }}</td>
-                    <td class="py-3 px-6 text-left">{{ $post->user->name }}</td>
-                    <td class="py-3 px-6 text-left">
-                        <a href="/posts/{{ $post->id }}/edit"
-                            class="inline-block bg-yellow-500 text-white py-2 px-4 rounded hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-yellow-500 dark:bg-yellow-600 dark:hover:bg-yellow-700">
-                            Edit
-                        </a>
-                        <a href="/posts/{{ $post->id }}"
-                            class="inline-block bg-green-500 text-white py-2 px-4 rounded hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 dark:bg-green-600 dark:hover:bg-green-700">
-                            Detail
-                        </a>
-                        <form action="/posts/{{ $post->id }}" method="post" class="inline-block">
-                            @csrf
-                            @method('delete')
-                            <button type="submit" onclick="return confirm('Are you sure?')"
-                                class="bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 dark:bg-red-600 dark:hover:bg-red-700">
-                                Delete
-                            </button>
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
+    {{-- <div class="flex flex-col"> --}}
+        <div class="overflow-x-auto">
+            <table class="min-w-full bg-white shadow-md rounded-lg overflow-hidden dark:bg-gray-800 dark:text-gray-100 mt-5">
+                <thead class="bg-blue-600 text-white dark:bg-blue-700">
+                    <tr>
+                        <th class="py-3 px-6 text-left">No. </th>
+                        <th class="py-3 px-6 text-left">Title</th>
+                        <th class="py-3 px-6 text-left">Slug</th>
+                        <th class="py-3 px-6 text-left">Content</th>
+                        <th class="py-3 px-6 text-left">Image</th>
+                        <th class="py-3 px-6 text-left">Category</th>
+                        <th class="py-3 px-6 text-left">User</th>
+                        <th class="py-3 px-6 text-left">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                        @if ($posts->count() > 0)
+                        {{-- Pagination --}}
+                        @foreach ($posts as $i => $post)
+                            <tr class="border-b dark:border-gray-600">
+                                <td class="py-3 px-6 text-left">{{ $i + 1 }}</td>
+                                <td class="py-3 px-6 text-left">{{ $post->title }}</td>
+                                <td class="py-3 px-6 text-left">{{ $post->slug }}</td>
+                                <td class="py-3 px-6 text-left">{{ \Illuminate\Support\Str::words($post->content, 3, '...') }}</td>
+                                <td class="py-3 px-6 text-left"><img src="{{ asset('/storage/posts/' . $post->image) }}" width="200" alt=""></td>
+                                <td class="py-3 px-6 text-left">{{ $post->category->name }}</td>
+                                <td class="py-3 px-6 text-left">{{ $post->user->name }}</td>
+                                <td class="py-3 px-6 text-left">
+                                    <a href="/posts/{{ $post->id }}/edit"
+                                        class="inline-block bg-yellow-500 text-white py-2 px-4 rounded hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-yellow-500 dark:bg-yellow-600 dark:hover:bg-yellow-700">
+                                        Edit
+                                    </a>
+                                    <a href="/posts/{{ $post->id }}"
+                                        class="inline-block bg-green-500 text-white py-2 px-4 rounded hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 dark:bg-green-600 dark:hover:bg-green-700">
+                                        Detail
+                                    </a>
+                                    <form action="/posts/{{ $post->id }}" method="post" class="inline-block">
+                                        @csrf
+                                        @method('delete')
+                                        <button type="submit" onclick="return confirm('Are you sure?')"
+                                            class="bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 dark:bg-red-600 dark:hover:bg-red-700">
+                                            Delete
+                                        </button>
+                                </td>
+                            </tr>
+                        @endforeach
+                        @else
+                           <tr>
+                               <td colspan="8" class="py-3 px-6 text-center">No data</td>
+                           </tr>
+                        @endif
+                    </tbody>
+                </table>
+        </div>
+    {{-- </div> --}}
 </div>
