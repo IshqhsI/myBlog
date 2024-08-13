1b5487536da33e8d9d48aa1ec3457b9667fc39d6
IshqhsI <oneridh18@gmail.com>
2024-08-13 14:04:02 +0800

    update & add method for search category

--- app/Livewire/CategoryComponent.php
+++ app/Livewire/CategoryComponent.php
@@ -4,13 +4,25 @@ namespace App\Livewire;
 
 use Livewire\Component;
 use App\Models\Category;
+use Livewire\Features\SupportTesting\Render;
 
 class CategoryComponent extends Component
 {
+    public $categories = [];
+    public $search = '';
     public function render()
     {
-        $categories = Category::all();
-        return view('livewire.category-component', compact('categories'));
+        $this->categories = Category::where('name', 'like', '%' . $this->search . '%')->orWhere('description', 'like', '%' . $this->search . '%')->get();
+        return view('livewire.category-component', [
+            'categories' => $this->categories
+        ]);
     }
 
+    public function searchCategory()
+    {
+        $this->categories = Category::where('name', 'like', '%' . $this->search . '%')->get();
+        return view('livewire.category-component', [
+            'categories' => $this->categories
+        ]);
+    }
 }
