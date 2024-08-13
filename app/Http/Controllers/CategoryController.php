f50228e94bcd51dd3ba2502ffbf3fd6b825fbf32
IshqhsI <oneridh18@gmail.com>
2024-08-13 14:06:32 +0800

    add method for show detail, edit, update and delete category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -30,4 +30,34 @@ class CategoryController extends Controller
 
         return redirect('/categories')->with('success', 'Category created successfully.');
     }
+
+    public function edit($id){
+        $category = Category::find($id);
+        return view('category.edit', compact('category'));
+    }
+
+    public function update(Request $request, $id){
+        $request->validate([
+            'name' => ['required', 'string', 'unique:categories,name,'.$id],
+            'description' => 'required',
+        ]);
+
+        $category = Category::find($id);
+        $category->name = $request->name;
+        $category->description = $request->description;
+        $category->save();
+
+        return redirect('/categories')->with('success', 'Category updated successfully.');
+    }
+
+    public function show($id){
+        $category = Category::find($id);
+        return view('category.show', compact('category'));
+    }
+
+    public function destroy($id){
+        $category = Category::find($id);
+        $category->delete();
+        return redirect('/categories')->with('success', 'Category deleted successfully.');
+    }
 }
