f0f3500e84243e6c772067e376a2725a169277cb
IshqhsI <oneridh18@gmail.com>
2024-08-14 14:21:20 +0800

    add method for store comment

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -16,4 +16,20 @@ class CommentController extends Controller
     public function create(){
         return view('comment.create');
     }
+
+    public function store(Request $request){
+        $request->validate([
+            'post_id' => 'required',
+            'comment_text' => 'required',
+        ]);
+
+        $comment = new Comment();
+        $comment->post_id = $request->post_id;
+        $comment->user_id = auth()->user()->id;
+        $comment->comment_text = $request->comment_text;
+        $comment->save();
+
+        return redirect('/comments')->with('success', 'Comment created successfully.');
+    }
+
 }
