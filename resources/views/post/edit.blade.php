c9a07636b7ea6e2535035f244927a370cc88ce65
IshqhsI <oneridh18@gmail.com>
2024-08-16 23:46:24 +0700

    add view edit post

---
+++ resources/views/post/edit.blade.php
@@ -0,0 +1,12 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Posts') }}
+        </h2>
+    </x-slot>
+    <div class="py-10">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            @livewire('edit-post-component', ['post' => $post, 'categories' => $categories, 'tags' => $tags])
+        </div>
+    </div>
+</x-app-layout>
