13a2ae0faf04519277dd26d109edc5406529385f
IshqhsI <oneridh18@gmail.com>
2024-08-23 19:44:10 +0800

    update route to about & contact

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,15 @@ use App\Http\Controllers\DashboardController;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
 
+Route::get('/about', function(){
+    return view('about');
+})->name('about');
+
+Route::get('/contact', function(){
+    return view('contact');
+})->name('contact');
 
 Route::get('/test', function () {
     return view('index');
