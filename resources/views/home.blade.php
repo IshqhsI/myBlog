e99b90ff10bef21142d8d8e5c64a0005b5ec890b
IshqhsI <oneridh18@gmail.com>
2024-08-31 22:01:46 +0800

    update hero section: change to general theme not just coding

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,14 +3,19 @@
     <main class="container mx-auto">
         <!-- Hero Section -->
         <section class="relative hero-bg h-96 md:h-96 flex items-center justify-center text-center text-white mb-8">
-            <div class="absolute inset-0 bg-black opacity-50"></div>
+            <div class="absolute inset-0 bg-black opacity-60"></div>
             <div class="relative z-10 p-6">
-                <h1 class="text-4xl md:text-5xl font-bold mb-4">Empowering Your Coding Journey</h1>
-                <p class="text-lg md:text-xl mb-6">Dive into insightful articles and tutorials to enhance your coding
-                    skills.</p>
+                <h1 class="text-4xl md:text-5xl font-bold mb-4 leading-tight">
+                    Explore Endless Inspiration and Insight
+                </h1>
+                <p class="text-lg md:text-xl mb-6 lg:max-w-6xl">
+                    Join us on a journey through a world of diverse and captivating content. From insightful articles to
+                    engaging stories, find something to spark your curiosity.
+                </p>
                 <a href="#newsletter"
-                    class="bg-indigo-600 text-white px-6 py-3 rounded-lg shadow-md hover:bg-indigo-700 transition duration-300">Subscribe
-                    Now</a>
+                    class="bg-indigo-600 text-white px-6 py-3 rounded-lg shadow-lg hover:bg-indigo-700 transition duration-300 transform hover:scale-105">
+                    Subscribe Now
+                </a>
             </div>
         </section>
 
@@ -18,7 +23,8 @@
 
         <!-- Featured Post -->
         <div class="bg-white dark:bg-gray-800 rounded-lg shadow-lg overflow-hidden mb-12 mx-4 md:mx-6">
-            <img src="{{ asset('storage/posts/' . $posts[0]->image) }}" alt="Featured Post" class="w-full h-80 object-cover">
+            <img src="{{ asset('storage/posts/' . $posts[0]->image) }}" alt="Featured Post"
+                class="w-full h-80 object-cover">
             <div class="p-6">
                 <h2 class="text-2xl font-semibold text-gray-800 dark:text-gray-100">{{ $posts[0]->title }}</h2>
                 <p class="mt-2 text-gray-600 dark:text-gray-400"> {!! \Illuminate\Support\Str::words(strip_tags($posts[0]->content), 30, '...') !!}</p>
