e7bf8e23e8447b7d7a5ab707908b0fec98e1ddc3
IshqhsI <oneridh18@gmail.com>
2024-09-12 22:30:14 +0800

    update view edit post: add argumen variabel statuses to post edit component

--- resources/views/post/edit.blade.php
+++ resources/views/post/edit.blade.php
@@ -6,7 +6,7 @@
     </x-slot>
     <div class="py-10">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            @livewire('edit-post-component', ['post' => $post, 'categories' => $categories, 'tags' => $tags])
+            @livewire('edit-post-component', ['post' => $post, 'categories' => $categories, 'tags' => $tags, 'statuses' => $statuses])
         </div>
     </div>
 </x-app-layout>
